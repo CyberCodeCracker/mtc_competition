b8273cf33c6042c6fd79d73f5a7460e5832520d9
CyberCodeCracker <souhailamouri123@gmail.com>
2026-01-31 22:58:02 +0100

    v3

--- backend/src/controllers/index.ts
+++
@@ -1,6 +0,0 @@
-export * as authController from './authController';
-export * as studentController from './studentController';
-export * as companyController from './companyController';
-export * as offerController from './offerController';
-export * as applicationController from './applicationController';
-export * as adminController from './adminController';
--- backend/src/middleware/index.ts
+++
@@ -1,3 +0,0 @@
-export { verifyToken, isAdmin, isCompany, isStudent, isAdminOrCompany, hasRole } from './auth';
-export { validate } from './validate';
-export { errorHandler, notFoundHandler, AppError } from './errorHandler';
